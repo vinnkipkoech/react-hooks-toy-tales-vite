04d5b3f0bd4a41fd0cf2714adce409948d644384
Vincent Kipkoech <vincent.kipkoech2@student.moringaschool.com>
2026-05-12 14:35:09 +0300

    Final fix for container and form

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toys, onDeleteToy, onUpdateToy }) {
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {/* Render the collection of ToyCards using map */}
+      {toys.map((toy) => (
+        <ToyCard 
+          key={toy.id} 
+          toy={toy} 
+          onDeleteToy={onDeleteToy} 
+          onUpdateToy={onUpdateToy} 
+        />
+      ))}
+    </div>
   );
 }
 
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -1,15 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ onAddToy }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+  });
+
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+    // Create the new toy object with initial likes set to 0
+    const newToy = {
+      ...formData,
+      likes: 0,
+    };
+
+    // POST request to backend
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newToy),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        onAddToy(data); // Update parent state
+        setFormData({ name: "", image: "" }); // Reset form
+      });
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={formData.name}
+          onChange={handleChange}
         />
         <br />
         <input
@@ -17,6 +55,8 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={formData.image}
+          onChange={handleChange}
         />
         <br />
         <input
